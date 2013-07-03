4ae943404138b951ee3863e6af2307953ea31c8e
Chris Fritz <chrisvfritz@gmail.com>
2013-07-03 16:11:31 -0400

    HEAD

--- lib/language_filter.rb
+++ lib/language_filter.rb
@@ -1,17 +1,23 @@
+require 'pathname'
+require 'yaml'
 require 'language_filter/error'
 require 'language_filter/version'
 
 module LanguageFilter
-	class LanguageFilter
+	class Test
 		attr_accessor :matchlist, :exceptionlist, :replacement
 
 		DEFAULT_EXCEPTIONLIST = []
-		DEFAULT_MATCHLIST = File.dirname(__FILE__) + "/../../config/filters/profanity.yml"
+		DEFAULT_MATCHLIST = File.dirname(__FILE__) + "/../config/filters/profanity.txt"
 		DEFAULT_REPLACEMENT = :stars
 
 		def initialize(options={})
-			@matchlist = set_list_content(options[:matchlist]) || DEFAULT_MATCHLIST
-			@exceptionlist = set_list_content(options[:exceptionlist]) || DEFAULT_EXCEPTIONLIST
+			@matchlist = if options[:matchlist] then
+				set_list_content(options[:matchlist])
+			else set_list_content(DEFAULT_MATCHLIST) end
+			@exceptionlist = if options[:exceptionlist] then
+				set_list_content(options[:exceptionlist]) 
+			else set_list_content(DEFAULT_EXCEPTIONLIST) end
 			@replacement = options[:replacement] || DEFAULT_REPLACEMENT
 			validate_options
 		end
@@ -84,8 +90,8 @@ module LanguageFilter
 		def validate_list_content(content)
 			case content
 			when Array    then not content.empty?    || raise(LanguageFilter::EmptyContentList.new("List content array is empty."))
-			when String   then File.exists?(content) || raise(LanguageFilter::UnkownContentFile.new("List content file can't be found."))
-			when Pathname then content.exist?        || raise(LanguageFilter::UnkownContentFile.new("List content file can't be found."))
+			when String   then File.exists?(content) || raise(LanguageFilter::UnkownContentFile.new("List content file \"#{content}\" can't be found."))
+			when Pathname then content.exist?        || raise(LanguageFilter::UnkownContentFile.new("List content file \"#{content}\" can't be found."))
 			when Symbol   then content == :default   || raise(LanguageFilter::UnkownContent.new("The only accepted symbol is :default."))
 			else raise LanguageFilter::UnkownContent.new("The list content can be either an Array, Pathname, or String path to a .yml file.")
 			end
@@ -102,14 +108,18 @@ module LanguageFilter
 
 		def set_list_content(list)
 			case list
-			when :hate      then YAML.load_file File.dirname(__FILE__) + "/../../config/filters/hate.yml"
-			when :profanity then YAML.load_file File.dirname(__FILE__) + "/../../config/filters/profanity.yml"
-			when :sex       then YAML.load_file File.dirname(__FILE__) + "/../../config/filters/sex.yml"
-			when :violence  then YAML.load_file File.dirname(__FILE__) + "/../../config/filters/violence.yml"
+			when :hate      then load_list File.dirname(__FILE__) + "/../config/filters/hate.yml"
+			when :profanity then load_list File.dirname(__FILE__) + "/../config/filters/profanity.txt"
+			when :sex       then load_list File.dirname(__FILE__) + "/../config/filters/sex.yml"
+			when :violence  then load_list File.dirname(__FILE__) + "/../config/filters/violence.yml"
 			when Array then list
-			when String, Pathname then YAML.load_file list.to_s
+			when String, Pathname then load_list list.to_s
 			else []
 			end
+		end
+
+		def load_list(filepath)
+			IO.readlines(filepath).each {|line| line.gsub!(/\n/,'')}
 		end
 
 		# This was moved to private because users should just use sanitize for any content
@@ -119,7 +129,7 @@ module LanguageFilter
 			when :stars  then '*' * word.size
 			when :nonconsonants then word.gsub(/[^bcdfghjklmnpqrstvwxyz]/i, '*')
 			when :default, :garbled then '$@!#%'
-			else raise LanguageFilter::UnknownReplacement.new("This is not a known replacement type.")
+			else raise LanguageFilter::UnknownReplacement.new("#{@replacement} is not a known replacement type.")
 			end
 		end
 	end
